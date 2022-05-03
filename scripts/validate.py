9830b9890f79f3b899cd515abba7b1be88e97a7f
Noah <konturn@wustl.edu>
2022-05-03 14:53:21 -0400

    feat: add validate step which outputs boolean used to determine whether plan should run

---
+++ scripts/validate.py
@@ -0,0 +1,11 @@
+import sys
+import os
+if __name__ == '__main__' :
+  with open('DEPLOYERS.md') as file:
+    deployers = [i.strip() for i in file]
+  is_fork = not os.environ['GITHUB_REPOSITORY'] == os.environ['PR_REPO_FULL_NAME']
+  is_deployer = os.environ['EMAIL'] in deployers
+  is_fork = False
+  is_deployer = True
+  valid_workflow = (is_fork and is_deployer and (os.environ['GITHUB_EVENT_NAME'] == 'pull_request')) or (not is_fork and (os.environ['GITHUB_EVENT_NAME'] == 'pull_request_target'))
+  print(valid_workflow)
